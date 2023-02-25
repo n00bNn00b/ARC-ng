734ca86bd86fed833a2a16e823e1f1eba1c9f398
n00bNn00b <dewanreal@gmail.com>
2023-02-25 20:26:08 +0600

    module not found issue fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Signup/Login/Login";
+import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 
 function App() {
