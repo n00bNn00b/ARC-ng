0b7c5cb149cd5420d398987b8c26c6e8adeebee5
n00bNn00b <dewanreal@gmail.com>
2023-02-26 19:51:02 +0600

    navbar with notification and sidebar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <input id="arcng-drwaer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
-        <div className="w-full navbar bg-primary">
-          <div className="flex-none lg:hidden">
-            <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+        <div className="w-full navbar bg-accent text-white">
+          <div className="flex-none">
+            <label htmlFor="arcng-drwaer" className="btn btn-square btn-ghost">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -24,22 +24,50 @@ const Navbar = () => {
             </label>
           </div>
           <div className="flex-1 px-2 mx-2">Arc-ng</div>
-          <div className="flex-none hidden lg:block">
-            <ul className="menu menu-horizontal">
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
-              <li>
-                <a>Navbar Item 2</a>
-              </li>
-            </ul>
+          <div className="flex lg:block dropdown dropdown-bottom dropdown-end">
+            <div className="navbar-end">
+              <button tabIndex={0} className="btn btn-ghost btn-circle">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                    />
+                  </svg>
+                  <span className="badge badge-xs badge-primary indicator-item"></span>
+                </div>
+              </button>
+            </div>
+            <div
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent rounded-box w-52 toast toast-top toast-end"
+            >
+              <div className="alert alert-success">
+                <div>
+                  <span>New mail arrived.</span>
+                </div>
+              </div>
+              <div className="alert alert-success">
+                <div>
+                  <span>Message sent successfully.</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         Content
       </div>
       <div className="drawer-side">
-        <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 bg-base-100">
+        <label htmlFor="arcng-drwaer" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 bg-accent text-white">
           <li>
             <a>Sidebar Item 1</a>
           </li>
