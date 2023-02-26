0498f31b3313a8d9d02d8adb8d368e772a0c8a98
n00bNn00b <dewanreal@gmail.com>
2023-02-26 17:29:18 +0600

    google login component has been separeted

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 import Signup from "./components/Signup/Signup";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/Divider/Divider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Divider = () => {
+  return (
+    <div>
+      <div className="divider">OR</div>
+    </div>
+  );
+};
+
+export default Divider;
