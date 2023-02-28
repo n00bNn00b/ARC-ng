6042c06a82ff4af81d8976853d1ff7d359dd0d8c
n00bNn00b <dewanreal@gmail.com>
2023-03-01 00:52:45 +0600

    mongodb connection established

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 5000 || process.env.PORT;
+require("dotenv").config();
+
+const DB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB Connected!"))
+  .catch((err) => console.log("DB not connected:\n", err));
 
 app.get("/", (req, res) => {
-  res.send("Diginventory Server is Running!");
+  res.send("Arc-ng Server is Running!");
 });
 
 app.listen(port, () => {
