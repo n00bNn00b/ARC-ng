04a5a8ef5ce8a4ab791fd49b1c25269dc3959388
n00bNn00b <dewanreal@gmail.com>
2023-10-13 23:33:18 +0600

    login api wrapper rewrited

--- server/router/testflask.js
+++ server/router/testflask.js
@@ -14,26 +14,31 @@ router.get("/flask_users", async (req, res) => {
 router.post("/flask_login", async (req, res) => {
   const { user_name, password } = req.body;
 
-  // Make a POST request to the Flask endpoint
   try {
     const response = await axios.post("http://127.0.0.1:5000/login", {
-      user_name: user_name,
-      password: password,
+      user_name,
+      password,
     });
-    console.log("responses: ", response);
+
+    if (response.status === 200) {
+      res.send(response.data);
+    } else {
+      res.status(response.status).send(response.data.error);
+    }
   } catch (error) {
     console.log(error);
+    res.status(500).send("Internal Server Error");
   }
-
-  // Check if the response was successful
-  // if (response.statusCode === 200) {
-  //   // The user was successfully logged in
-  //   res.status(200).json({ message: "User successfully logged in", response });
-  // } else {
-  //   // There was an error logging in the user
-  //   res.status(401).json({ error: "There was an error logging in the user" });
-  // }
 });
+
+// Check if the response was successful
+// if (response.statusCode === 200) {
+//   // The user was successfully logged in
+//   res.status(200).json({ message: "User successfully logged in", response });
+// } else {
+//   // There was an error logging in the user
+//   res.status(401).json({ error: "There was an error logging in the user" });
+// }
 
 router.get("/flask_user/:id", async (req, res) => {
   const id = req.params.id;
